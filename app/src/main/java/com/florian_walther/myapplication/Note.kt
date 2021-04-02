24da0f5888a2379ef940c9463fbde58124f53de4
My Nguyen <nguyen_my@yahoo.com>
2021-04-01 17:09:32 -0700

    Part 2: Room entity

---
+++ app/src/main/java/com/florian_walther/myapplication/Note.kt
@@ -0,0 +1,8 @@
+package com.florian_walther.myapplication
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class Note(@PrimaryKey(autoGenerate=true) val id: Int=0,
+                val title: String, val description: String, val priority: Int)
