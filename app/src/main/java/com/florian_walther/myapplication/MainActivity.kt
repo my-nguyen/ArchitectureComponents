335f7d397c9e79e96d30048da304678a2f0a883f
My Nguyen <nguyen_my@yahoo.com>
2021-04-01 20:19:41 -0700

    Part 6: RecyclerView & Adapter

--- app/src/main/java/com/florian_walther/myapplication/MainActivity.kt
+++ app/src/main/java/com/florian_walther/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.florian_walther.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +17,18 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val noteAdapter = NoteAdapter()
+        binding.rvNotes.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = noteAdapter
+        }
+
         noteViewModelFactory = NoteViewModelFactory(application)
         noteViewModel = ViewModelProvider(this, noteViewModelFactory).get(NoteViewModel::class.java)
         noteViewModel.getAll().observe(this, {
             // update RecyclerView
-            Toast.makeText(this@MainActivity, "onChanged", Toast.LENGTH_SHORT).show()
+            noteAdapter.setNotes(it)
         })
     }
 }
---
+++ app/src/main/java/com/florian_walther/myapplication/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.florian_walther.myapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.florian_walther.myapplication.databinding.ItemNoteBinding
+
+class NoteAdapter(): RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+    private lateinit var notes: List<Note>
+
+    class ViewHolder(private val binding: ItemNoteBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
+            binding.apply {
+                tvTitle.text = note.title
+                tvDescription.text = note.description
+                tvPriority.text = note.priority.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemNoteBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = notes[position]
+        holder.bind(note)
+    }
+
+    override fun getItemCount() = notes.size
+
+    fun setNotes(notes: List<Note>) {
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+}
